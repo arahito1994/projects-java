7e4102ef5db2f38455646fe01e974249c6b82c4b
arahito1994 <artht0408@gmail.com>
2022-02-24 14:52:36 +0900

    LendMoneyPrinter

---
+++ project02/src/LendMoneyPrinter.java
@@ -0,0 +1,8 @@
+public class LendMoneyPrinter {
+  public static void main (String[] args) {
+    String name = "山本一郎";
+    int money = 100;
+
+    System.out.print(name + "さん、" + money + "円を貸してください");
+  }
+}
