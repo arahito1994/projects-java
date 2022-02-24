548a9148b0778702ab7f2ad521c87fb9e52a8ad8
arahito1994 <artht0408@gmail.com>
2022-02-24 14:26:27 +0900

    StringLinePrinter

---
+++ project02/src/StringLinePrinter.java
@@ -0,0 +1,12 @@
+public class StringLinePrinter {
+  public static void main(String[] args) {
+    String firstBlock = "AAA";
+    String secondBlock = "bbbb";
+    String thirdBlock = "CCCCC";
+
+    System.out.print(firstBlock);
+    System.out.print(secondBlock);
+    System.out.print(thirdBlock);
+}
+}
+
