5f9b70ccd31e76d6aeebaf05ef3b050f8e8bf899
arahito1994 <artht0408@gmail.com>
2022-02-24 14:14:12 +0900

    SelfIntroduction

---
+++ project02/src/SelfIntroduction.java
@@ -0,0 +1,9 @@
+public class SelfIntroduction {
+    public static void main(String[] args) {
+        String myName = "山本一郎";
+        int age = 757;
+    
+        System.out.println("私は" + myName + "です");
+        System.out.println("年齢は" + age + "歳です");
+    }
+}
